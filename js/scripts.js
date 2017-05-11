6db8c2182da34810bfd1700fb26a377191336f5b
Alexander <mburukungu@gmail.com>
2017-05-11 11:37:55 +0300

    placedd sript

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+  $("#add-address").click(function(){
+    $("#new-address").append('<div class="new-address">' +
+                              '<div class="form-group">' +
+                                '<label for="new-street">Street</label>' +
+                                '<input type="text" class="form-contol new-street">' +
+                              '</div>' +
+
+                              '<div class="form-group">' +
+                                '<label for="new-city">City</label>' +
+                                '<input type="text" class="form-contol new-city">' +
+                              '</div>' +
+
+                              '<div class="form-group">' +
+                                '<label for="new-county">County</label>' +
+                                '<input type="text" class="form-contol new-county">' +
+                              '</div>' +
+                            '<div>');
+
+
+  });
+});
+
+$(".new-address").each(function(){
+  var inputtedStreet = $(this).find("input.new-street").val();
+  var inputtedCity = $(this).find("input.new-city").val();
+  var inputtedCounty = $(this).find("input.new-county").val();
+  var newAddress = new Address(inputtedStreet, inputtedCity, inputtedCounty);
+  newContact.address.push(newAddress);
+});
